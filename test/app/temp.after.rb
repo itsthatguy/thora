6b9168ec1fb61ab55581d4396e58da9aa4ea2769
Kevin Altman <kevin@itsthatguy.com>
2015-03-15 16:38:26 -0400

    test tmp files

---
+++ test/app/temp.after.rb
@@ -0,0 +1,49 @@
+require File.expand_path('../boot', __FILE__)
+
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+# require "rails/test_unit/railtie"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Thora
+  class Application < Rails::Application
+    # Register our bower assets with the Asset pipeline
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
+
+    # Tell browserify_rails to use coffeeify
+    config.browserify_rails.commandline_options = [
+      "-t coffeeify",
+      "-t bulkify",
+      "--extension=\".js.coffee\""
+    ]
+
+    # Should the node_modules directory be evaluated for changes on page load
+    #
+    # The default is `false`
+    config.browserify_rails.evaluate_node_modules = true
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # config.time_zone = 'Central Time (US & Canada)'
+
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # config.i18n.default_locale = :de
+
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
+  end
+end
